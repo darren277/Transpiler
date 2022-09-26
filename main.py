192f2d4035d619953846ccdc189422348c63d103
Darren <darren277@yahoo.com>
2022-09-26 08:49:23 -0700

    Commented out unecessary print statements.

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ class Code:
     def pre_hook(self, loc):
         func_name = loc['func'].__name__
         e = loc['args'][0]
-        print("PRE HOOK", func_name, e, loc)
-        print(e.__dir__())
-        print(convert_back_to_code(self.code, e))
+        #print("PRE HOOK", func_name, e, loc)
+        #print(convert_back_to_code(self.code, e))
 
     def post_hook(self, loc):
-        print("POST HOOK", loc['func'].__name__, loc['args'][0], loc)
+        #print("POST HOOK", loc['func'].__name__, loc['args'][0], loc)
+        ...
 
     def log(self, msg: str):
         ...
