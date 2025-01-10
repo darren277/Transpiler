12117d731ea8250070b4ae425ab2019985e454c6
darren277 <darren277@yahoo.com>
2025-01-10 03:16:39 -0800

    New logger.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """"""
 from src.hooks import Hooks
-from src.logging import Logging
+from src.logger import Logger
 from src.main import Code
 from src.parsing.main import Visitor
 
 
-class Main(Code, Visitor, Hooks, Logging):
+class Main(Code, Visitor, Hooks, Logger):
     def throw(self, e: str):
         raise Exception(e)
 
