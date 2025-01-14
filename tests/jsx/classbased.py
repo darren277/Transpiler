7706c0efecb0c30edfab4fbd2d6535f0b7ec3039
darren277 <darren277@yahoo.com>
2025-01-14 05:41:20 -0800

    Class based React.

---
+++ tests/jsx/classbased.py
@@ -0,0 +1,30 @@
+""""""
+
+class AppWithProps:
+    def __init__(self, props):
+        self.props = props
+
+    def render(self):
+        return (Div({self.props.author.name}, className="App"))
+
+
+# class Welcome extends React.Component
+class AppWithState(React.Component):
+    def __init__(self):
+        self.state = {
+            "name": "John",
+            "age": 30
+        }
+
+    def changeName(self, name):
+        self.setState({"name": name})
+
+    def render(self):
+        return (
+            Div(
+                H1("Hello ", self.state["name"]),
+                H1("Your age: ", self.state["age"]),
+                Button("Change name", onClick=self.changeName("Bob")),
+                Button("Change age", onClick=self.setState({"age": 50})),
+            )
+        )
