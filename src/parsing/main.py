292c3a75636dcabca09d8ab2d462ddb6443c72ba
darren277 <darren277@yahoo.com>
2025-01-15 06:53:12 -0800

    Conversion of all references to `self` into `this`.

--- src/parsing/main.py
+++ src/parsing/main.py
@@ -141,6 +141,8 @@ class Visitor:
     @pre_hook_wrapper
     @post_hook_wrapper
     def process_attribute_call(self, call: ast.Call, s: str = "") -> str:
+        if call.func.value.id == 'self':
+            call.func.value.id = 'this'
         try:
             call_func = call.func
         except:
