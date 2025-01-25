68ec47e5ce3dcd60ca6c1d28c3d37c12bc8fd5cd
darren277 <darren277@yahoo.com>
2025-01-25 05:12:16 -0800

    Whoops forgot to call lower().

--- src/parsing/main.py
+++ src/parsing/main.py
@@ -185,7 +185,7 @@ class Visitor:
         return (function_name in self.imported_components) or (function_name in self.defined_classes) or (function_name in self.defined_functions)
 
     def is_react_component(self, function_name: str):
-        return (((function_name.lower == 'Fragment'.lower()) or (function_name.lower() in HTML_TAGS) or self.is_already_defined(function_name)))
+        return (((function_name.lower() == 'Fragment'.lower()) or (function_name.lower() in HTML_TAGS) or self.is_already_defined(function_name)))
 
     @pre_hook_wrapper
     @post_hook_wrapper
