0c9a84184499013c379c9a26ded38862770dad81
darren277 <darren277@yahoo.com>
2025-02-12 08:21:23 -0800

    Multifile test case.

--- tests/jsx_tests.py
+++ tests/jsx_tests.py
@@ -46,4 +46,65 @@ export default App;
 #test_loaded_file_with_run(None, simple_app)
 
 #test_loaded_file_with_run('tests/jsx/classbased.py')
-test_loaded_file_with_run('tests/jsx/functionbased.py')
+#test_loaded_file_with_run('tests/jsx/functionbased.py')
+
+
+
+def transpile_file(root_path: str, file_path: str):
+    js = Main(open(file_path, 'r').read())
+    js.config.react_app = True
+    js.config.wrap_return = "()"
+
+    js.register_event_handler('handleEditClick')
+    js.register_event_handler('deleteUser')
+
+    s = js.transpile()
+    print(s)
+    actual_file_path = file_path.replace(root_path, '').replace('.py', '.jsx').replace('\\', '/')
+
+    # create subdir if it doesn't exist
+    subdir = os.path.dirname(f'tests/testout/multifile/src{actual_file_path}')
+    if not os.path.exists(subdir):
+        os.makedirs(subdir)
+
+    with open(f'tests/testout/multifile/src{actual_file_path}', 'w') as f:
+        print("ABOUT TO WRITE:", f'tests/testout/multifile/src{actual_file_path}')
+        f.write(s)
+
+def multifile_test(path: str):
+    import os
+
+    # for file in os.listdir(path):
+    #     if file.endswith('.py'):
+    #         print(f"Testing {file}...")
+    #         transpile_file(path, os.path.join(path, file))
+
+    # recursive...
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.py'):
+                print(f"Testing {file}...")
+                transpile_file(path, os.path.join(root, file))
+
+    # prettify via CLI...
+    p = subprocess.Popen('npm run prettier', stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='tests/testout/multifile', shell=True)
+    out = p.stdout.read()
+    print(out)
+    err = p.stderr.read().decode()
+    if err:
+        print(err)
+        exception = next(line for line in err.splitlines() if line.startswith('SyntaxError'))
+        raise Exception(exception)
+    print(err)
+
+    # p = subprocess.Popen('npm run start', stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='tests/testout/multifile', shell=True)
+    # out = p.stdout.read()
+    # print(out)
+    # err = p.stderr.read().decode()
+    # if err:
+    #     print(err)
+    #     exception = next(line for line in err.splitlines() if line.startswith('SyntaxError'))
+    #     raise Exception(exception)
+    # print(err)
+
+multifile_test('tests/jsx/multifile')
